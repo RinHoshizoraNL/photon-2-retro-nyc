6d8c4d93b3f639c30cfb04c577504683c0ac8611
Rin Hoshizora <hoshizora-designs@outlook.com>
2026-03-20 21:45:52 +0100

    89 Caprice configs

---
+++ lua/photon-v2/library/vehicles/nypd_photon2_rin_chevy_caprice89_highway.lua
@@ -0,0 +1,456 @@
+if (Photon2.ReloadVehicleFile()) then return end
+local VEHICLE = Photon2.LibraryVehicle()
+
+VEHICLE.Title 		= "1989 Chevrolet Caprice NYPD Highway Patrol"
+VEHICLE.Vehicle		= "caprice_civ_sgm"
+VEHICLE.Category 	= "Photon 2 NYC: NYPD"
+VEHICLE.Author		= "Rin Hoshizora"
+
+local sequence = Photon2.SequenceBuilder.New
+
+local WHEEL_X		= -1
+
+VEHICLE.Equipment = {
+	{
+		Category = "Livery",
+		Options = {
+			{
+				Option = "Highway Patrol 1989",
+				SubMaterials = {
+					{ Id = 7, Material = "rin/nypd/caprice89/highway_89" },
+				},
+			},
+		}
+	},
+	{
+		Category = "Lightbar",
+		Options = {
+            {
+				Option = "Federal Signal Aerodynic",
+				Components = {
+					{
+                        Component = "fedsig_aerodynicny",
+                        Position = Vector( 0, -4, 61.4 ),
+                        Angles = Angle( 0, 180, 0 ),
+                        Scale = 1.06,
+					},
+				}
+			},
+        }
+	},
+	{
+		Category = "Highriser",
+		Options = {
+			{
+				Option = "Highriser",
+				Props = {
+					{
+						Model = "models/tdmcars/emergency/equipment/mcdermott_riser.mdl",
+						Position = Vector( 0, -25, 63.5 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 0.95,
+						SubMaterials = {
+							[0] = "rin/nypd/props/white",
+						},
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( -24, -15.6, 59.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 3, 3, 1.5 ),
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( 24, -15.6, 59.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 3, 3, 1.5 ),
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( -24, -34.4, 59.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 3, 3, 1.5 ),
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( 24, -34.4, 59.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 3, 3, 1.5 ),
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( 2.5, -19, 69 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 8.2, 0.2, 0.44 ),
+						SubMaterials = {
+							[0] = "rin/nypd/props/white",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( -2.5, -31, 69 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 8.2, 0.2, 0.44 ),
+						SubMaterials = {
+							[0] = "rin/nypd/props/white",
+						},
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( -8, -20.2, 70 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 1, 1, 1 ),
+						SubMaterials = {
+							[0] = "sentry/shared/env_cubemap_model",
+						},
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( 8, -29.7, 70 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 1, 1, 1 ),
+						SubMaterials = {
+							[0] = "sentry/shared/env_cubemap_model",
+						},
+					},
+				},
+				Components = {
+					{
+						Component = "photon_mars_flasher_nypd",
+						Position = Vector( 21.4, -20.2, 68.8 ),
+						Angles = Angle( 90, 0, 0 ),
+						Scale = 1,
+					},
+					{
+						Component = "photon_mars_flasher_nypd",
+						Position = Vector( -21.4, -29.7, 68.8 ),
+						Angles = Angle( -90, 0, 0 ),
+						Scale = 1,
+						Phase = 180,
+					},
+					{
+						Component = "photon_mars_flasher_nypd",
+						Position = Vector( 20, -20.2, 64.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+						Phase = 180,
+					},
+					{
+						Component = "photon_mars_flasher_nypd",
+						Position = Vector( -20, -29.7, 64.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+					},
+					{
+						Component = "photon_highrise_spotlight_nypd",
+						Position = Vector( -8, -20.2, 72 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+					},
+					{
+						Component = "photon_highrise_spotlight_nypd",
+						Position = Vector( 8, -29.7, 72 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+					},
+				}
+			}
+		}
+	},
+	{
+		Category = "Rear Deck Lights",
+		Options = {
+            {
+				Option = "Rear Deck Lights",
+				Components = {
+					{
+						Component = "photon_par46_flasher_nypd",
+						Position = Vector( -12, -54, 45 ),
+						Angles = Angle( 0, 180, 0 ),
+						Scale = 0.7,
+						Phase = 180,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Component = "photon_par46_flasher_nypd",
+						Position = Vector( 12, -54, 45 ),
+						Angles = Angle( 0, 180, 0 ),
+						Scale = 0.7,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+                },
+				Props = {
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( -12, -54.1, 41.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 0.7, 0.7, 1.5 ),
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( 12, -54.1, 41.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 0.7, 0.7, 1.5 ),
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+				},
+			},
+			{
+				Option = "None",
+			},
+		}
+	},
+	{
+		Category = "Siren",
+		Options = {
+			{
+				Option = "Scientific Prototypes Mark-VII",
+				Components = {
+					{
+						Name = "@siren_speaker",
+						Component = "siren_prototype",
+						Model = "models/gandhi/props/es100.mdl",
+						Position = Vector( 0, 100, 30 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1,
+						Siren = "sp_mk7",
+						Templates = {
+							["Sound"] = { 
+								Tone = {
+									DSP = 0,
+									Pitch = 100
+								}
+							}
+						},
+						Inputs = { 
+							["Emergency.SirenParkKill"] = { ["PARK"] = {} }
+						}
+					},
+				}
+			}
+		}
+	},
+	{
+		Category = "Spotlight",
+		Options = {
+			{
+				Option = "Spotlight",
+				Components = {
+					{
+						Component = "photon_par46_left",
+						Position = Vector( -37, 31.5, 47 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+						SubMaterials = {
+							[5] = "sentry/shared/env_cubemap_model"
+						}
+					},
+				}
+			},
+		}
+	},
+	{
+		Category = "Police Equipment",
+		Options = {
+			{
+				Option = "Police Equipment",
+				Props = {
+					{
+						Model = "models/gandhi/props/radio.mdl",
+						Position = Vector( 0, 34, 27.3 ),
+						Angles = Angle( 270, 270, 0 ),
+						Scale = 0.32,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/gandhi/props/mastercom.mdl",
+						Position = Vector( 0, 31, 28),
+						Angles = Angle( 90, 270, 0 ),
+						Scale = 1.17,
+						SubMaterials = {
+							[0] = "rin/nypd/props/sp_mk7/sp_mark7",
+						},
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/sentry/props/setina_partition_cvpi.mdl",
+						Position = Vector( 0, 9, -2),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 1, 1, 0.97),
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/schmal/antenna_vhf_1.mdl",
+						Position = Vector( 39.5, 50, 38 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 1, 1, 0.7 ),
+					},
+					{
+						Model = "models/schmal/antenna_vhf_2.mdl",
+						Position = Vector( 0, -90, 39.5 ),
+						Angles = Angle( 0, 0, 7 ),
+						Scale = Vector( 1, 1, 1 ),
+					},
+					{
+						Model = "models/xenosprops/na_plate/na_plate_gov.mdl",
+						Position = Vector( 0, -113, 28.8 ),
+						Angles = Angle( 0, 270, 00 ),
+						Scale = 1,
+						SubMaterials = {
+							[1] = "rin/nypd/props/plates/plate_patrol95",
+						},
+					},
+					{
+						Model = "models/xenosprops/na_plate/na_plate_gov.mdl",
+						Position = Vector( 0, 119.45, 15 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 0.99,
+						SubMaterials = {
+							[1] = "rin/nypd/props/plates/plate_patrol95",
+						},
+					},
+				}
+			}
+		}
+	},
+	{
+		Category = "Pushbar",
+		Options = {
+			{
+				Option = "Pushbar",
+				Props = {
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( 13.5, 122, 24.5),
+						Angles = Angle( 7, 0, 0 ),
+						Scale = Vector( 0.65, 0.2, 3.6),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( 13.5, 117, 23.3),
+						Angles = Angle( 0, 0, 90 ),
+						Scale = Vector( 0.5, 0.05, 2),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( 12.5, 117, 15),
+						Angles = Angle( 0, 0, 90 ),
+						Scale = Vector( 0.5, 0.05, 2),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/misc/tubes/size_1/h_tube_12x6.mdl",
+						Position = Vector( 13.5, 123.5, 24.5),
+						Angles = Angle( 7, 0, 270 ),
+						Scale = Vector( 0.3, 3.5, 0.4),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( -13.5, 122, 24.5),
+						Angles = Angle( -7, 0, 0 ),
+						Scale = Vector( 0.65, 0.2, 3.6),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( -13.5, 117, 23.3),
+						Angles = Angle( 0, 0, 90 ),
+						Scale = Vector( 0.5, 0.05, 2),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( -12.5, 117, 15),
+						Angles = Angle( 0, 0, 90 ),
+						Scale = Vector( 0.5, 0.05, 2),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/misc/tubes/size_1/h_tube_12x6.mdl",
+						Position = Vector( -13.5, 123.5, 24.5),
+						Angles = Angle( -7, 0, 270 ),
+						Scale = Vector( 0.3, 3.5, 0.4),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+				}
+			},
+		}
+	},
+	{
+		Category = "Wheels",
+		Options = {
+			{
+				Option = "Hub Caps",
+				Bones = {
+					{ Bone = "fl_wheel", Scale = 0, Position = Vector(0, 0, 0), Angles = Angle(0, 0, 0) },
+					{ Bone = "fr_wheel", Scale = 0, Position = Vector(0, 0, 0), Angles = Angle(0, 0, 0) },
+					{ Bone = "bl_wheel", Scale = 0, Position = Vector(0, 0, 0), Angles = Angle(0, 0, 0) },
+					{ Bone = "br_wheel", Scale = 0, Position = Vector(0, 0, 0), Angles = Angle(0, 0, 0) },
+				},
+				Props = {
+					{
+						Model = "models/lilranbay/vehicle_wheels/caprice89_hubcaps.mdl",
+						Position = Vector( 0, 0, 0 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+						Bones = {
+							["bone_wheel_front_left"] = { 
+								Position = Vector( WHEEL_X, 0, 0 ),
+								Angles = Angle( 0, 0, 0 ),
+								Scale = 1,
+								Follow = { Attachment = "wheel_fl" }
+							},
+							["bone_wheel_front_right"] = { 
+								Position = Vector( WHEEL_X, 0, 0 ),
+								Angles = Angle( 0, 0, 0 ),
+								Scale = 1,
+								Follow = { Attachment = "wheel_fr" }
+							},
+							["bone_wheel_rear_left"] = { 
+								Position = Vector( WHEEL_X, 0, 0 ),
+								Angles = Angle( 0, 0, 0 ),
+								Scale = 1,
+								Follow = { Attachment = "wheel_rl" }
+							},
+							["bone_wheel_rear_right"] = { 
+								Position = Vector( WHEEL_X, 0, 0 ),
+								Angles = Angle( 0, 0, 0 ),
+								Scale = 1,
+								Follow = { Attachment = "wheel_rr" }
+							}
+						}
+					}
+				}
+			},
+			{
+				Option = "Steelies",
+				Bones = {
+					{ Bone = "fl_wheel", Scale = 1, Position = Vector(0, 0, 0), Angles = Angle(0, 0, 0) },
+					{ Bone = "fr_wheel", Scale = 1, Position = Vector(0, 0, 0), Angles = Angle(0, 0, 0) },
+					{ Bone = "bl_wheel", Scale = 1, Position = Vector(0, 0, 0), Angles = Angle(0, 0, 0) },
+					{ Bone = "br_wheel", Scale = 1, Position = Vector(0, 0, 0), Angles = Angle(0, 0, 0) },
+				}
+			}
+		}
+	}
+}
