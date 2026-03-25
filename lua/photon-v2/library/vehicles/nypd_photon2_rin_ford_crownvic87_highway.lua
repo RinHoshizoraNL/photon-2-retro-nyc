ca2492b80c6538f6b6a778f345f1e6d0942bce83
Rin Hoshizora <hoshizora-designs@outlook.com>
2026-03-25 16:43:05 +0100

    NYPD 87 CrownVic

---
+++ lua/photon-v2/library/vehicles/nypd_photon2_rin_ford_crownvic87_highway.lua
@@ -0,0 +1,412 @@
+if (Photon2.ReloadVehicleFile()) then return end
+local VEHICLE = Photon2.LibraryVehicle()
+
+VEHICLE.Title 		= "1987 Ford Crown Victoria NYPD Highway Patrol"
+VEHICLE.Vehicle		= "87ltd_sgm"
+VEHICLE.Category 	= "Photon 2 NYC: NYPD"
+VEHICLE.Author		= "Rin Hoshizora"
+
+local sequence = Photon2.SequenceBuilder.New
+
+VEHICLE.Equipment = {
+    {
+		Category = "Livery",
+		Options = {
+			{
+				Option = "Highway Patrol 1987",
+				SubMaterials = {
+					{ Id = 2, Material = "rin/nypd/crownvic87/seats_blue" },
+					{ Id = 3, Material = "rin/nypd/crownvic87/doors_blue" },
+					{ Id = 6, Material = "rin/nypd/crownvic87/dash_blue" },
+					{ Id = 7, Material = "rin/nypd/crownvic87/highway_87" },
+					{ Id = 15, Material = "rin/nypd/crownvic87/wheel_blue" },
+				},
+			},
+		}
+	},
+	{
+		Category = "Lightbar",
+		Options = {
+            {
+				Option = "Federal Signal Aerodynic",
+				Components = {
+					{
+                        Component = "fedsig_aerodynicny",
+                        Position = Vector( 0, -10, 59.8 ),
+                        Angles = Angle( 0, 180, 0 ),
+                        Scale = 1.06,
+					},
+				}
+			},
+        }
+	},
+	{
+		Category = "Grille Lights",
+		Options = {
+            {
+				Option = "Grille Lights",
+				Components = {
+					{
+						Component = "photon_par46_flasher_nypd",
+						Position = Vector( -7, 102, 25 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 0.7,
+						Phase = 180,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Component = "photon_par46_flasher_nypd",
+						Position = Vector( 7, 102, 22 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 0.7,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+                },
+			},
+			{
+				Option = "None",
+			},
+		}
+	},
+	{
+		Category = "Highriser",
+		Options = {
+			{
+				Option = "Highriser",
+				Props = {
+					{
+						Model = "models/tdmcars/emergency/equipment/mcdermott_riser.mdl",
+						Position = Vector( 0, -30, 61.5 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 0.95,
+						SubMaterials = {
+							[0] = "rin/nypd/props/white",
+						},
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( -24, -20.6, 57.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 3, 3, 1.5 ),
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( 24, -20.6, 57.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 3, 3, 1.5 ),
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( -24, -39.4, 57.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 3, 3, 1.5 ),
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( 24, -39.4, 57.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 3, 3, 1.5 ),
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( 2.5, -24, 67 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 8.2, 0.2, 0.44 ),
+						SubMaterials = {
+							[0] = "rin/nypd/props/white",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( -2.5, -36, 67 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 8.2, 0.2, 0.44 ),
+						SubMaterials = {
+							[0] = "rin/nypd/props/white",
+						},
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( -8, -25.2, 68 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 1, 1, 1 ),
+						SubMaterials = {
+							[0] = "sentry/shared/env_cubemap_model",
+						},
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( 8, -34.7, 68 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 1, 1, 1 ),
+						SubMaterials = {
+							[0] = "sentry/shared/env_cubemap_model",
+						},
+					},
+				},
+				Components = {
+					{
+						Component = "photon_mars_flasher_nypd",
+						Position = Vector( 21.4, -25.2, 66.8 ),
+						Angles = Angle( 90, 0, 0 ),
+						Scale = 1,
+					},
+					{
+						Component = "photon_mars_flasher_nypd",
+						Position = Vector( -21.4, -34.7, 66.8 ),
+						Angles = Angle( -90, 0, 0 ),
+						Scale = 1,
+						Phase = 180,
+					},
+					{
+						Component = "photon_mars_flasher_nypd",
+						Position = Vector( 20, -25.2, 62.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+						Phase = 180,
+					},
+					{
+						Component = "photon_mars_flasher_nypd",
+						Position = Vector( -20, -34.7, 62.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+					},
+					{
+						Component = "photon_highrise_spotlight_nypd",
+						Position = Vector( -8, -25.2, 70 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+					},
+					{
+						Component = "photon_highrise_spotlight_nypd",
+						Position = Vector( 8, -34.7, 70 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+					},
+				}
+			}
+		}
+	},
+	{
+		Category = "Spotlight",
+		Options = {
+			{
+				Option = "Spotlight",
+				Components = {
+					{
+						Component = "photon_par46_left",
+						Position = Vector( -38, 25, 42 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+						SubMaterials = {
+							[5] = "sentry/shared/env_cubemap_model"
+						}
+					},
+				}
+			},
+		}
+	},
+	{
+		Category = "Siren",
+		Options = {
+			{
+				Option = "Scientific Prototypes Mark-VII",
+				Components = {
+					{
+						Name = "@siren_speaker",
+						Component = "siren_prototype",
+						Model = "models/gandhi/props/es100.mdl",
+						Position = Vector( 0, 100, 30 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1,
+						Siren = "sp_mk7",
+						Templates = {
+							["Sound"] = { 
+								Tone = {
+									DSP = 0,
+									Pitch = 100
+								}
+							}
+						},
+						Inputs = { 
+							["Emergency.SirenParkKill"] = { ["PARK"] = {} }
+						}
+					},
+				}
+			}
+		}
+	},
+	{
+		Category = "Additional Brake Lights",
+		Options = {
+            {
+				Option = "Additional Brake Lights",
+				Components = {
+					{
+						Component = "photon_par46_brake_nypd",
+						Position = Vector( -25, -70, 42 ),
+						Angles = Angle( 0, 180, 0 ),
+						Scale = 0.7,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Component = "photon_par46_brake_nypd",
+						Position = Vector( 25, -70, 42 ),
+						Angles = Angle( 0, 180, 0 ),
+						Scale = 0.7,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+                },
+				Props = {
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( -25, -70, 38.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 0.7, 0.7, 1.5 ),
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/schmal/antenna_data.mdl",
+						Position = Vector( 25, -70, 38.5 ),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = Vector( 0.7, 0.7, 1.5 ),
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+				},
+			},
+		}
+	},
+	{
+		Category = "Police Equipment",
+		Options = {
+			{
+				Option = "Police Equipment",
+				Props = {
+					{
+						Model = "models/sentry/props/setina_partition_cvpi.mdl",
+						Position = Vector( 0, 0, -5),
+						Angles = Angle( 0, 0, 0 ),
+						Scale = 1,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/gandhi/props/mastercom.mdl",
+						Position = Vector( 0, 23, 32.5),
+						Angles = Angle( 90, 270, 0 ),
+						Scale = 1.15,
+						SubMaterials = {
+							[0] = "rin/nypd/props/sp_mk7/sp_mark7",
+						},
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/supermighty/photon/front_holder.mdl",
+						Position = Vector( -34, 111.2, 16.8 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1
+					},
+					{
+						Model = "models/xenosprops/na_plate/na_plate_gov.mdl",
+						Position = Vector( 0, -123, 30 ),
+						Angles = Angle( -16, 270, 0 ),
+						Scale = 1,
+						SubMaterials = {
+							[1] = "rin/nypd/props/plates/plate_patrol95",
+						},
+					},
+					{
+						Model = "models/xenosprops/na_plate/na_plate_gov.mdl",
+						Position = Vector( -34, 112, 16.8 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 0.99,
+						SubMaterials = {
+							[1] = "rin/nypd/props/plates/plate_patrol95",
+						},
+					},
+				}
+			}
+		}
+	},
+	{
+		Category = "Pushbar",
+		Options = {
+			{
+				Option = "Pushbar",
+				Props = {
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( 13.5, 116, 22.5),
+						Angles = Angle( 7, 0, 0 ),
+						Scale = Vector( 0.65, 0.2, 3.6),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( 13.5, 110, 21.8),
+						Angles = Angle( 0, 0, 90 ),
+						Scale = Vector( 0.5, 0.05, 2),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( 12.5, 110, 12),
+						Angles = Angle( 0, 0, 90 ),
+						Scale = Vector( 0.5, 0.05, 2),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/misc/tubes/size_1/h_tube_12x6.mdl",
+						Position = Vector( 13.5, 117.5, 22.5),
+						Angles = Angle( 7, 0, 270 ),
+						Scale = Vector( 0.3, 3.5, 0.4),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( -13.5, 116, 22.5),
+						Angles = Angle( -7, 0, 0 ),
+						Scale = Vector( 0.65, 0.2, 3.6),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( -13.5, 110, 21.8),
+						Angles = Angle( 0, 0, 90 ),
+						Scale = Vector( 0.5, 0.05, 2),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/cuboids/height06/size_1/cube_6x6x6.mdl",
+						Position = Vector( -12.5, 110, 12),
+						Angles = Angle( 0, 0, 90 ),
+						Scale = Vector( 0.5, 0.05, 2),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+					{
+						Model = "models/sprops/misc/tubes/size_1/h_tube_12x6.mdl",
+						Position = Vector( -13.5, 117.5, 22.5),
+						Angles = Angle( -7, 0, 270 ),
+						Scale = Vector( 0.3, 3.5, 0.4),
+						SubMaterials = {
+							[0] = "sentry/dippy/black",
+						},
+					},
+				}
+			},
+		}
+	},
+}
---
+++ lua/photon-v2/library/vehicles/nypd_photon2_rin_gmc_vandura83.lua
@@ -0,0 +1,105 @@
+if (Photon2.ReloadVehicleFile()) then return end
+local VEHICLE = Photon2.LibraryVehicle()
+
+VEHICLE.Title 		= "1983 GMC Vandura NYPD"
+VEHICLE.Vehicle		= "vandura1983"
+VEHICLE.Category 	= "Photon 2 NYC: NYPD"
+VEHICLE.Author		= "Rin Hoshizora"
+
+local sequence = Photon2.SequenceBuilder.New
+
+VEHICLE.SubMaterials = {
+    [3] = "sentry/shared/env_cubemap_model",
+	[9] = "sentry/dippy/white_squares",
+	[12] = "sentry/shared/glass",
+	[13] = "sentry/dippy/amber_squares",
+	[15] = "sentry/dippy/red_squares",
+}
+
+VEHICLE.Equipment = {
+    {
+		Category = "Livery",
+		Options = {
+			{
+				Option = "Patrol",
+				SubMaterials = {
+					{ Id = 11, Material = "rin/nypd/vandura83/patrol_83" },
+				},
+			},
+		}
+	},
+	{
+		Category = "Lightbar",
+		Options = {
+            {
+				Option = "Federal Signal Aerodynic",
+				Components = {
+					{
+                        Component = "fedsig_aerodynicny",
+                        Position = Vector( 0, 27, 102.5 ),
+                        Angles = Angle( 0, 180, 0 ),
+                        Scale = 1.02,
+					},
+				}
+			},
+        }
+	},
+	{
+		Category = "Siren",
+		Options = {
+			{
+				Option = "Scientific Prototypes Mark-VII",
+				Components = {
+					{
+						Name = "@siren_speaker",
+						Component = "siren_prototype",
+						Model = "models/gandhi/props/es100.mdl",
+						Position = Vector( 0, 95, 50 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1,
+						Siren = "sp_mk7",
+						Templates = {
+							["Sound"] = { 
+								Tone = {
+									DSP = 0,
+									Pitch = 100
+								}
+							}
+						},
+						Inputs = { 
+							["Emergency.SirenParkKill"] = { ["PARK"] = {} }
+						}
+					},
+				}
+			}
+		}
+	},
+	{
+		Category = "Police Equipment",
+		Options = {
+			{
+				Option = "Police Equipment",
+				Props = {
+					{
+						Model = "models/xenosprops/na_plate/na_plate_gov.mdl",
+						Position = Vector( 0, -119.1, 35.2 ),
+						Angles = Angle( -8, 270, 0 ),
+						Scale = 1,
+						SubMaterials = {
+							[1] = "rin/nypd/props/plates/plate_patrol95",
+						},
+					},
+					{
+						Model = "models/xenosprops/na_plate/na_plate_gov.mdl",
+						Position = Vector( 0, 102.4, 36.3 ),
+						Angles = Angle( 4, 90, 0 ),
+						Scale = 1,
+						SubMaterials = {
+							[1] = "rin/nypd/props/plates/plate_patrol95",
+						},
+					},
+				}
+			}
+		}
+	},
+}
